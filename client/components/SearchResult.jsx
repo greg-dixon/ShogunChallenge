c6dc39bebf73bcad506ec15b6211628b13e23eb9
greg-dixon <gdixon529@gmail.com>
2021-03-19 16:54:31 -0700

    created overall file structure and boilerplate code for server and frontend

---
+++ client/components/SearchResult.jsx
@@ -0,0 +1,11 @@
+const SearchResult = () => {
+
+    return (
+        <div>
+            test
+        </div>
+    )
+
+}
+
+export default SearchResult
---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+          <h1>Address Search</h1>
+      <input type="text" placeholder="1234 Appleseed Lane"></input>
+
+    </div>
+  );
+}
+
+export default App;
